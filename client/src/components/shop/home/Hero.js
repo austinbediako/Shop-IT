dd579bf614765e352a36d5758d292517aebbfc15
austinbediako <austinbediako4@gmail.com>
2026-03-23 13:46:19 +0000

    fixes

--- client/src/components/shop/home/Hero.js
+++ client/src/components/shop/home/Hero.js
@@ -1,8 +1,6 @@
 import React, { Fragment } from "react";
 import OrderSuccessMessage from "./OrderSuccessMessage";
 
-const apiURL = process.env.REACT_APP_API_URL;
-
 const Hero = () => {
 
   return (
--- client/src/components/shop/home/ProductCategoryDropdown.js
+++ client/src/components/shop/home/ProductCategoryDropdown.js
@@ -5,8 +5,6 @@ import { getAllCategory } from "../../admin/categories/FetchApi";
 import { getAllProduct, productByPrice } from "../../admin/products/FetchApi";
 import "./style.css";
 
-const apiURL = process.env.REACT_APP_API_URL;
-
 const CategoryList = () => {
   const history = useHistory();
   const [categories, setCategories] = useState(null);
